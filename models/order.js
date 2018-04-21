ae8357b3348a25d88437f6783a7557c12cb634f9
krosenk729 <krosenk729@gmail.com>
2018-04-21 12:08:40 -0400

    mock data progress

--- models/order.js
+++ models/order.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const orderSchema = new Schema({
-  forWho: String,
-  items: [{
-  	_id: String,
-  	name: String,
-  	price: Number,
-  	quantity: Number
-  }],
-  date: { type: Date, default: Date.now }
+	forWho: String,
+	items: [{
+		name: String,
+		price: Number,
+		quantity: Number,
+		image: String
+	}],
+	date: { type: Date, default: Date.now }
 });
 
 const Order = mongoose.model("Order", orderSchema);
