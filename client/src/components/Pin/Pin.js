4e4b38984f43dd0ffb3bb2f1b92081f03735fd3e
krosenk729 <krosenk729@gmail.com>
2018-04-24 20:50:42 -0400

    adding pin page

---
+++ client/src/components/Pin/Pin.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+// import API from "../utils/API";
+
+class Pin extends Component {
+  state = {
+    pinIsValid
+  };
+
+  componentDidMount() {
+  }
+
+  handleBtnClick = event => {
+    this.setState(newState);
+    this.loadNextDog();
+  };
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default Pin;
