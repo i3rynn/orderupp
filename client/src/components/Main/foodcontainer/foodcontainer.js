e076c4f17ef9df55a4d1a9d5d83c23811980b876
krosenk729 <krosenk729@gmail.com>
2018-04-26 11:02:26 -0400

    cleaning up jsx

---
+++ client/src/components/Main/foodcontainer/foodcontainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./foodcontainer.css";
+
+const foodcontainer = props => (
+	<React.Fragment>
+	<img src="http://via.placeholder.com/285x285" alt="placeholder" className="img-thumbnail" />
+
+	<div className="card">
+	<div className="card-body">
+	<h5 className="card-title">{props.ingredientTitle}</h5>
+	<p className="card-text">{props.ingredientText}</p>
+	<button href="#" className="btn btn-primary">Submit Order</button>
+	</div>
+	</div>
+	</React.Fragment>
+);
+
+export default foodcontainer;
