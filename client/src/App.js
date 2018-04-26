934d3862f74a60fc50a38dc06cfa1ca7a50bdd3c
krosenk729 <krosenk729@gmail.com>
2018-04-26 19:28:45 -0400

    removing pin

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Pin from "./components/Pin";
 import Main from "./components/Main";
 
 const App = () => (
   <Router>
     <div>
       <Switch>
-        <Route exact path="/order" component={Main} />
-        <Route component={Pin} />
+        <Route component={Main} />
       </Switch>
     </div>
   </Router>
