315e69ccb6dd4a3050b9ccd962c17e8984c790cc
aoutland <melia.miimii@gmail>
2018-04-26 21:24:57 -0400

    order component

---
+++ client/src/components/Main/order/order.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+var order = React.createClass({
+  render: function() {
+    return (
+
+      <div className="added-orders" style={{width: '35%', height: 100, float: 'right', marginTop: '2%', textAlign: 'center'}}> <i>Tap the circle below to remove item from your order list<i>
+            <img src="http://via.placeholder.com/300x300" style={{width: 100, borderRadius: '50%', margin: 5}} alt="Avatar" />  
+            <img src="http://via.placeholder.com/300x300" style={{width: 100, borderRadius: '50%', margin: 5}} alt="Avatar" />  
+            <img src="http://via.placeholder.com/300x300" style={{width: 100, borderRadius: '50%', margin: 5}} alt="Avatar" />	
+            <img src="http://via.placeholder.com/300x300" style={{width: 100, borderRadius: '50%', margin: 5}} alt="Avatar" />	
+            <img src="http://via.placeholder.com/300x300" style={{width: 100, borderRadius: '50%', margin: 5}} alt="Avatar" />	
+            <img src="http://via.placeholder.com/300x300" style={{width: 100, borderRadius: '50%', margin: 5}} alt="Avatar" />	
+            <img src="http://via.placeholder.com/300x300" style={{width: 100, borderRadius: '50%', margin: 5}} alt="Avatar" />	
+          </i></i></div>
+    );
+  }
+});
