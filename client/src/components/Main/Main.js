580964e30d3bd9f6a6eed625110647def8ce6dfa
krosenk729 <krosenk729@gmail.com>
2018-04-28 10:52:06 -0400

    setting placeholder

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -7,9 +7,9 @@ import Chooser from "./chooser";
 
 class Main extends Component {
   state = {
-    menuoptions: {},
-    orderitems: [],
-    currentView: {}
+    menuOptions: {},
+    orderItems: [],
+    currentSelection: {}
   };
 
   displaySpecial = category => {
@@ -22,24 +22,33 @@ class Main extends Component {
     .then(menuoptions => {
     	this.setState({menuoptions});
     });
+
+    // set currentSelection to placeholder values
+    let placeholder = {
+      _id: +new Date(),
+      category: "",
+      name: "Select an item from above...",
+      image: "http://via.placeholder.com/285x285",
+      price: 0,
+      desc: ""
+    }
+    this.setState({currentSelection: placeholder});
+    
   }
 
   render() {
     return (
       <React.Fragment>
-      <Chooser menuoptions={this.state.menuoptions} />
-      {this.state.currentView ? 
-        <Foodcontainer {...this.state.currentView} /> :
-        null
-      }
-      <div>
-      <Foodcontainer />
+      <div className="headerArea">
+      <Chooser menuOptions={this.state.menuOptions} />
       </div>
-      <Order orderitems={this.state.orderitems} />
-      <div>
-      Total: {this.state.orderitems.reduce((a, t) => a + t, 0)}
+      <div className="orderArea">
+      <Foodcontainer {...this.state.currentSelection} />
+      <Order orderItems={this.state.orderItems} />
       </div>
-
+      <div className="totalArea">
+        Total: {this.state.orderItems.reduce((a, t) => a + t, 0)}
+      </div>
       </React.Fragment>
       );
   }
