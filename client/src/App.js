2ea2947c4f4621e6a94574e619a62260c286def0
krosenk729 <krosenk729@gmail.com>
2018-04-28 10:17:22 -0400

    updating react routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./components/Main";
+import NotFound from "./components/NotFound";
 
 const App = () => (
   <Router>
     <div>
       <Switch>
-        <Route component={Main} />
+		<Route path="/order" component={Main} />
+		<Redirect from="/" to="/order" />
+        <Route component={NotFound} />
       </Switch>
     </div>
   </Router>
