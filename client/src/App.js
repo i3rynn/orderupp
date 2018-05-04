7bd1b9b8ce2e0afd20d83cd055fd0738895e01b5
davidl199 <davidl1_99@yahoo.com>
2018-05-04 14:50:51 -0400

    Implemented authentication into app, only issue is the order screen is loaded twice

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,52 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+// import React from "react";
+// import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+// import Main from "./components/Main";
+// import NotFound from "./components/NotFound";
+
+// const App = () => (
+//   <Router>
+//       <Switch>
+// 		<Route path="/order" component={Main} />
+// 		<Redirect from="/" to="/order" />
+//         <Route component={NotFound} />
+//       </Switch>
+//   </Router>
+// );
+
+//export default App;
+
+///////////////////////////////////////////////////////////////////////////////////////////
+import React from 'react';
+import { Route, Router } from 'react-router-dom';
+import AppLogin from './AppLogin';
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
+import Home from './Home/Home';
+//import Order from './Order/Order';
+import Callback from './Callback/Callback';
+import Auth from './Auth/Auth';
+import history from './history';
 
-const App = () => (
-  <Router>
-      <Switch>
-		<Route path="/order" component={Main} />
-		<Redirect from="/" to="/order" />
-        <Route component={NotFound} />
-      </Switch>
-  </Router>
-);
+const auth = new Auth();
 
-export default App;
+const handleAuthentication = ({location}) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
+export const makeMainRoutes = () => {
+  return (
+      <Router history={history}>
+        <div>
+          <Route path="/" render={(props) => <AppLogin auth={auth} {...props} />} />
+          <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
+          <Route path="/order" render={(props) => <Main auth={auth} {...props} />} />
+          <Route path="/callback" render={(props) => {
+            handleAuthentication(props);
+            return <Callback {...props} /> 
+          }}/>
+        </div>
+      </Router>
+  );
+}
