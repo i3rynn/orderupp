9304a9174df95adc22f2e36529a55354ee1d0aaa
davidl199 <davidl1_99@yahoo.com>
2018-05-05 12:20:38 -0400

    Cleaned up some comments

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -93,38 +93,3 @@ class Main extends Component {
 }
 
 export default Main;
-
-// <div>
-//         <h3>Order page</h3>
-//         </div>
-
-// <React.Fragment>
-// <div className="menuSelection mb-4">
-//   <Chooser menuOptions={this.state.menuOptions} setCurrent={this.setCurrent} />
-// </div>
-// <div className="orderArea w-100 d-flex justify-content-between">
-
-//   {this.state.currentSelection ?
-//     <Foodcontainer {...this.state.currentSelection} addToOrder={this.addCurrentToOrder} /> :
-//     null}
-
-//   <Order orderItems={this.state.orderItems} removeOrder={this.removeFromOrder} />
-// </div>
-// <div className="totalArea mt-4 display-4">
-//   Total: {total}
-// </div>
-// <div className="menuSelection mb-4">
-//   <Chooser menuOptions={this.state.menuOptions} setCurrent={this.setCurrent} />
-// </div>
-// <div className="orderArea w-100 d-flex justify-content-between">
-
-//   {this.state.currentSelection ?
-//     <Foodcontainer {...this.state.currentSelection} addToOrder={this.addCurrentToOrder} /> :
-//     null}
-
-//   <Order orderItems={this.state.orderItems} removeOrder={this.removeFromOrder} />
-// </div>
-// <div className="totalArea mt-4 display-4">
-//   Total: {total}
-// </div>
-// </React.Fragment>
