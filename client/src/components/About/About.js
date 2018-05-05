285e8ab9e35f28f73a113eedb01a56ee420af31c
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-05-05 12:26:14 -0400

    updates to about

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -7,7 +7,7 @@ const About = () => (
   <h1>Meet the Staff</h1>
   <div className="card">
     <div className="img-container">
-      <img alt=  src= />
+      <img alt="chris" src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/16831972_3667198354819_2949335325848159760_n.jpg?_nc_cat=0&oh=2c09e35949efbfd9ddc46b19cef5672a&oe=5B8E656B" />
     </div>
     <div className="content">
       <ul>
