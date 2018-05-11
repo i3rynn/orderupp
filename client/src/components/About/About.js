7efd551fc0052c773c03b580f39f461b73f6916e
Chris Yang <chrisyang@Chriss-MacBook-Pro.local>
2018-05-11 16:39:25 -0400

    changes

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import './About.css';
-=======
-import logo from './logo.svg';
-import './App.css';
->>>>>>> 50acbb40b2c76b4a7bdbf8d998f2d850aa438caf
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
 
       <div classNameName="row">
         <div>
@@ -23,25 +17,11 @@ class App extends Component {
               <h3 className="title">Executive Chef</h3>
               <p id="bio">Executive chef. At the young age of 26, David has already established a strong name for himself
           in the restaurant industry. He is a professional cook who is proficient in all aspects of food preparation.</p>
-=======
-      <div classNameName="row">
-
-        <div className="column">
-          <div className="card">
-            <img src="Headshots/jermaine.jpg" alt="Jermaine" />
-            <div className="container">
-              <h2>Jermaine Foster</h2>
-              <h3 className="title">Master Dishwasher</h3>
-              <p>Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem.
-              </p>
-              <p><button className="button">Contact</button></p>
->>>>>>> 50acbb40b2c76b4a7bdbf8d998f2d850aa438caf
             </div>
           </div>
         </div>
 
         <div className="column">
-<<<<<<< HEAD
           <div className="cardAbout">
             <img src="Headshots/Amelia.jpg" alt="Amelia" class="imgAbout" />
             <div className="container">
@@ -49,21 +29,11 @@ class App extends Component {
               <h3 className="title">Sous Chef</h3>
               <p id="bio">A Philadelphia native, Amelia has fond memories of learning about delicious home cooking with her family in
                 Philadelphia, Georgia and Turks and Caicos. She began working in a string of New York City restaurants: The Tasty Grub where she was promoted to sous chef after just four months on the job. </p>
-=======
-          <div className="card">
-            <img src="Headshots/amelia.jpg" alt="Amelia" />
-            <div className="container">
-              <h2>Amelia Outland</h2>
-              <h3 className="title">Sous Chef</h3>
-              <p> Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p><button className="button">Contact</button></p>
->>>>>>> 50acbb40b2c76b4a7bdbf8d998f2d850aa438caf
             </div>
           </div>
         </div>
 
         <div className="column">
-<<<<<<< HEAD
           <div className="cardAbout">
             <img src="Headshots/image2.jpeg" alt="Chris" class="imgAbout" />
             <div className="container">
@@ -71,21 +41,11 @@ class App extends Component {
               <h3 className="title">Pastry Chef</h3>
               <p id="bio"> Chris is the greatest pastry chef on planet earth. He has won numerous awards and has been recognized for his greatness at producing the best desserts.
                 Apart from working here, he is currently the top Iron Chef on Iron Chef America, specializing on French dessert.</p>
-=======
-          <div className="card">
-            <img src="Headshots/katherine.jpeg" alt="Katherine" />
-            <div className="container">
-              <h2>Katherine Rosenkranz</h2>
-              <h3 className="title">Custodian</h3>
-              <p>Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p><button className="button">Contact</button></p>
->>>>>>> 50acbb40b2c76b4a7bdbf8d998f2d850aa438caf
             </div>
           </div>
         </div>
 
         <div className="column">
-<<<<<<< HEAD
           <div className="cardAbout">
             <img src="Headshots/jermaine.jpg" alt="Jermaine" class="imgAbout" />
             <div className="container">
@@ -94,42 +54,22 @@ class App extends Component {
               <p id="bio">Jermaine Foster, 37. Master Dishwasher a.k.a. The King of Cutlery a.k.a Silver Palms. This guy has the quickest hands in the Southeast,
                 shuffling plates and pots like playing cards at lightning fast speed as though the were playing cards.
               </p>
-=======
-          <div className="card">
-            <img src="Headshots/amelia.jpg" alt="Amelia" />
-            <div className="container">
-              <h2>Amelia Outland</h2>
-              <h3 className="title">Sous Chef</h3>
-              <p> A Philadelphia native, Amelia has fond memories of learning about delicious home cooking with her family in Philadelphia, Georgia and Turks and Caicos. She began working in a string of New York City restaurants: The Tasty Grub where she was promoted to sous chef after just four months on the job.</p>
-              <p><button className="button">Contact</button></p>
->>>>>>> 50acbb40b2c76b4a7bdbf8d998f2d850aa438caf
             </div>
           </div>
         </div>
 
         <div className="column">
-<<<<<<< HEAD
           <div className="cardAbout">
             <img src="Headshots/image1.jpeg" alt="Katherine" class="imgAbout" />
             <div className="container">
               <h2 id="nameTag">Katherine Rosenkranz</h2>
               <h3 className="title">Custodian</h3>
               <p id="bio">Katherine is an okay custodian. Not bad, not great either, kind of neutral, lukewarm, middle of the pack.</p>
-=======
-          <div className="card">
-            <img src="Headshots/image2.jpeg" alt="Chris" />
-            <div className="container">
-              <h2>Chris Yang</h2>
-              <h3 className="title">Pastry Chef</h3>
-              <p> Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p><button className="button">Contact</button></p>
->>>>>>> 50acbb40b2c76b4a7bdbf8d998f2d850aa438caf
             </div>
           </div>
         </div>
 
         <div className="column">
-<<<<<<< HEAD
           <div className="cardAbout">
             <img src="Headshots/thomas.jpg" alt="Amelia" class="imgAbout" />
             <div className="container">
@@ -138,21 +78,10 @@ class App extends Component {
               <p id="bio"> Thomas Gentle is a recent graduate of Georgia Tech with a degree in Business Administration IT Management. Thomas
                 is pursuing an entrepreneur career path and is currently leading his second startup. He has a desire to learn how to code
                 so that he can better manage and lead a team of developers.</p>
-=======
-          <div className="card">
-            <img src="Headshots/DavidL.PNG" alt="David" />
-            <div className="container">
-              <h2>David Lester</h2>
-              <h3 className="title">Executive Chef</h3>
-              <p>Executive chef. At the young age of 26, David has already established a strong name for himself
-          in the restaurant industry. He is a professional cook who is proficient in all aspects of food preparation.</p>
-              <p><button className="button">Contact</button></p>
->>>>>>> 50acbb40b2c76b4a7bdbf8d998f2d850aa438caf
             </div>
           </div>
         </div>
 
-<<<<<<< HEAD
         <div className="column">
           <div className="cardAbout">
             <img src="Headshots/marsalis.jpg" alt="Marsalis" class="imgAbout" />
@@ -161,16 +90,6 @@ class App extends Component {
               <h3 className="title">Manager</h3>
               <p id="bio">Deciding to try his hand at something new, Marsalis applied to be the manager of his own
                 restaurant. While everything is still relatively new, leading a team and making people happy is a life goal he never knew he had.</p>
-=======
-                <div className="column">
-          <div className="card">
-            <img src="Headshots/marsalis.jpg" alt="Marsalis" />
-            <div className="container">
-              <h2>Marsalis Sanders</h2>
-              <h3 className="title">Manager</h3>
-              <p>Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem. Some text that describes me lorem ipsum ipsum lorem.</p>
-              <p><button className="button">Contact</button></p>
->>>>>>> 50acbb40b2c76b4a7bdbf8d998f2d850aa438caf
             </div>
           </div>
         </div>
