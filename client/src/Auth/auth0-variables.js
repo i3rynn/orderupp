1f51296953994ee1fa00afeddac8672d1f8c987a
krosenk729 <krosenk729@gmail.com>
2018-05-11 18:20:51 -0400

    testing heroku

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'reactbuilder.auth0.com',
   clientId: 'oD5ZdunOBVaSjpBaCZFUd37dkwdoxd9y',
-  callbackUrl: 'https://restaurant-thing-project-thing.herokuapp.com/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
